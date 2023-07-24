e1752f718eb0915f2934bb9adf04b44d4d05a7b1
EvgenySp <Evgeniu07@yandex.ru>
2023-07-24 23:04:11 +0300

    НАчало ?

---
+++ src/DayZ.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DayZ {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите год:");
+            int year = scanner.nextInt();
+
+
+            if (isLeapYear(year)) {
+                System.out.println("Пьем 12.09." + year);
+                // здесь нужно вывести результат
+            }
+            else {
+                System.out.println("Пьем 13.09." + year);
+            }
+        }
+        public static boolean isLeapYear(int year) {
+            if (year%4 == 0 && year%400 ==0){
+                return true;
+            }
+            else     // здесь нужно определить, является ли переданный год високосным
+                return false;
+        }
+    }
+
